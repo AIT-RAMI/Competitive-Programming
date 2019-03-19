482761efc611338587c9a3a4e446372fb2003516
AIT RAMI Mohamed <43639743+AIT-RAMI@users.noreply.github.com>
2019-03-19 16:52:09 +0100

    Update Main.java

--- Codeforces Round #547/Game 23/Main.java
+++ Codeforces Round #547/Game 23/Main.java
@@ -7,7 +7,10 @@ public class Main {
         int b = S.nextInt();
         int d = b/a;
         int result = 0;
-        if ( (b%a != 0 || (d%2 !=0 && d%3 != 0)&& d != 1 )) {
+        if (d == 1) {
+            System.out.println(0);
+        } 
+        if ( b%a != 0 || (d%2 !=0 && d%3 != 0)) {
             System.out.println(-1);
         } else {
             while ( d%2 == 0) {
