e244cfe7fe8e0328598c0f1a382c5652b2c8e95b
AIT RAMI Mohamed <43639743+AIT-RAMI@users.noreply.github.com>
2019-03-19 16:39:17 +0100

    Create Main.java

---
+++ Codeforces Round #547/Game 23/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int a = S.nextInt();
+        int b = S.nextInt();
+        int d = b/a;
+        int result = 0;
+        if ( (b%a != 0 || (d%2 !=0 && d%3 != 0)) && d != 1 ) {
+            System.out.println(-1);
+        } else {
+            while ( d%2 == 0) {
+                d = d/2;
+                result += 1;
+            }
+            while ( d%3 == 0) {
+                d = d/3;
+                result += 1;
+            }
+            System.out.println(result);
+        }
+    }
+}
