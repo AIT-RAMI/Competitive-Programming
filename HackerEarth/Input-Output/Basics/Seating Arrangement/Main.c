e97ed1e993cd8818ace635b3fda996d3d403537e
AIT RAMI Mohamed <43639743+AIT-RAMI@users.noreply.github.com>
2019-03-20 16:31:26 +0100

    Update Main.c

--- HackerEarth/Input-Output/Basics/Seating Arrangement/Main.c
+++ HackerEarth/Input-Output/Basics/Seating Arrangement/Main.c
@@ -2,22 +2,14 @@
 int main(int argc, const char *argv[])
 {   int N;
     scanf("%d",&N);
-    int Numbers[N];
+    unsigned long long Numbers[N];
     for (int i = 0; i < N; i++) {
-        scanf("%d",Numbers+i);
+        scanf("%ld",Numbers+i);
     }
+    unsigned long long result = 1;
     for (int i = 0; i < N; i++) {
-        int n = Numbers[i] % 12;
-        if (n == 0) n = 12;
-        printf("%d",Numbers[i] - 2*n + 13);
-        n = n % 6;
-        if (n == 0 || n == 1) {
-            printf(" WS\n");
-        } else if (n == 5 || n == 2) {
-            printf(" MS\n");
-        } else {
-            printf(" AS\n");
-        }
+        result = ( (result  % 1000000007 ) * (Numbers[i] % 1000000007) ) % 1000000007;
     }
+    printf("%d", result);
     return 0;
 }
