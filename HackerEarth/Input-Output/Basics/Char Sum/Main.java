764e7b2186659fbbed979c59d7244618711532b7
AIT RAMI Mohamed <43639743+AIT-RAMI@users.noreply.github.com>
2019-03-31 00:29:42 +0100

    Create Main.java

---
+++ HackerEarth/Input-Output/Basics/Char Sum/Main.java
@@ -0,0 +1,51 @@
+import java.math.*;
+import java.util.*;
+import java.io.*;
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreElements()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException  e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    public static void main(String[] args) {
+        FastReader S = new FastReader();
+        String word = S.next();
+        char[] wordChar = word.toCharArray();
+        int ans = 0;
+        for (int i = 0; i < wordChar.length; i++) {
+            ans += wordChar[i] - 'a' + 1;
+        }
+        System.out.println(ans);
+    }
+}
