499ea020cd9fe34059574b0634c419d95da7ab61
AIT RAMI Mohamed <43639743+AIT-RAMI@users.noreply.github.com>
2019-03-30 01:59:32 +0100

    Update _Main.java

--- Ensias/Random Mashup #1/B. Anastasia and pebbles/_Main.java
+++ Ensias/Random Mashup #1/B. Anastasia and pebbles/_Main.java
@@ -43,25 +43,39 @@ public class _Main {
     public static void main(String[] args) {
         FastReader S = new FastReader();
         int n = S.nextInt();
-        int m = S.nextInt();
-        int min = n / 2;
-        if (n % 2 == 0) {
-            while (min % m != 0 && min <= n) {
-                min += 1;
-                if (min % m == 0) {
-                    break;
+        int k = S.nextInt();
+        int []w = new int[n];
+        for (int i = 0; i < n; i++){
+            w[i] = S.nextInt();
+        }
+        long d = 0;
+        for (int i = 0; i < n; i++){
+            double temp = (double) w[i] / k;
+            if (temp == (double) Math.floor(temp)) d+= temp;
+            else d += Math.floor(temp) + 1;
+        }
+        if (d % 2 == 0) System.out.println(d/2);
+        else System.out.println((d/2) + 1);
+
+/*
+        int type = 0;
+        int quantity = 0;
+        int days = 0;
+        while (type < n) {
+            days += 1;
+            if (w[type] > quantity) quantity += k;
+            else {
+                type += 1;
+                quantity = 0;
+            }
+            if (type < n) {
+                if (w[type] > quantity) quantity += k;
+                else {
+                    type += 1;
+                    quantity = 0;
                 }
             }
-        } else {
-            do {
-                min += 1;
-                if (min % m == 0) {
-                    break;
-                }
-            } while (min % m != 0 && min <= n);
         }
-
-        if (min <= n) System.out.println(min);
-        else System.out.println(-1);
+        System.out.println(days);*/
     }
 }
