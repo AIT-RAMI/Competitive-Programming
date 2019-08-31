b8d2cc124bb26724c6b56d3cae22b698634de6e8
AIT RAMI Mohamed <43639743+AIT-RAMI@users.noreply.github.com>
2019-08-31 01:37:35 +0100

    Create ez.cpp

---
+++ HackerEarth/Data Structures/Hash Tables/The Monk and Kundan/ez.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1000000007;
+
+#define pf(n) cout << n << endl
+#define pfs(n) cout << n
+#define ps() cout <<" ";
+#define sf(n) cin >> n
+#define in(i,n) for (int i = 0; i < n; i++)
+#define ni(i,n) for (int i = n - 1; i >= 0; i--)
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define graph vector< vector<int> >
+#define vs vector<string>
+/**  '' "Lionel VB" ^ **/
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll T;
+    sf(T);
+    string letter ="abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cin.ignore();
+    while(T--){
+        ll ans = 0;
+        int index = 0;
+        int num = 1;
+        string line;
+        getline(cin, line);
+        in(i, line.size()){
+            if (line[i] == ' ') {
+                index = 0;
+                num++;
+            }
+            else {
+                ans += letter.find(line[i]);
+                ans += index;
+                index++;
+            }
+        }
+        pf(ans*num);
+    }
+    return 0;
+
+}
