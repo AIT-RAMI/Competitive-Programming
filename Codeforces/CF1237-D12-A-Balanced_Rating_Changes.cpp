5ecf1e9af41b3694ebd19082b030efdeb4435593
AIT-RAMI <mohamedaitrami@gmail.com>
2020-02-20 23:44:42 +0100

    CF1237-D12-A-Balanced_Rating_Changes

---
+++ Codeforces/CF1237-D12-A-Balanced_Rating_Changes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1000000007;
+
+#define pf(n) cout << n << endl
+#define pfs(n) cout << n
+#define ps() cout << " "
+#define sf(n) cin >> n
+#define in(i,n) for (int i = 0; i < n; i++)
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define graph vector< vector<int> >
+#define vs vector<string>
+const int MAXN = 100005;
+/**  '' "Lionel VB" ^ **/
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, a;
+    cin >> n;
+    int pdebt = 0;
+    int ndebt = 0;
+    in(i, n){
+        cin >> a;
+        if (a % 2 == 0) cout << a / 2 << endl;
+        else {
+            if(a > 0){
+                cout << (a / 2) + pdebt << endl;
+                pdebt ^= 1;
+            } 
+            else {
+                cout << (a / 2) - ndebt << endl;
+                ndebt ^= 1;
+            }
+        }
+    }
+
+    return 0;
+}
