352371920f6b71cbc552c737bad9af52bfd037ec
AIT-RAMI <mohamedaitrami@gmail.com>
2020-04-14 17:15:10 +0100

    Adding NumberSpiral

---
+++ CSES/NumberSpiral.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1000000007;
+
+#define pf(n) cout << n << endl
+#define pfs(n) cout << n
+#define ps() cout << " "
+#define sf(n) cin >> n
+#define in(i,n) for (int i = 0; i < n; i++)
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define graph vector< vector<int> >
+#define vs vector<string>
+const int MAXN = 100005;
+/**  '' "Lionel VB" ^ **/
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t, x, y;
+    cin >> t;
+    while(t--){
+        cin >> y >> x;
+        ll mx = max(x, y);
+        ll mn = min(x, y);
+        if(mx % 2 == 0){
+            if(x > y){
+                cout << (mx - 1)*(mx - 1) + mn << endl;
+            } else {
+                cout << mx*mx - mn + 1 << endl;
+            }
+        } else {
+            if(x < y){
+                cout << (mx - 1)*(mx - 1) + mn << endl;
+            } else {
+                cout << mx*mx - mn + 1 << endl;
+            }
+        }
+    }
+    return 0;
+}
