e6e2e4d38f46bafcc5f4d27adc0fc24a6113d172
AIT-RAMI <mohamedaitrami@gmail.com>
2020-04-14 01:19:16 +0100

    MissingNumber

---
+++ CSES/MissingNumber.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1000000007;
+
+#define pf(n) cout << n << endl
+#define pfs(n) cout << n
+#define ps() cout << " "
+#define sf(n) cin >> n
+#define in(i,n) for (int i = 0; i < n; i++)
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define graph vector< vector<int> >
+#define vs vector<string>
+const int MAXN = 100005;
+/**  '' "Lionel VB" ^ **/
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n, t, ans;
+    sf(n);
+    ans = n*(n+1)/2;
+    in(i, n - 1) {
+        sf(t);
+        ans -= t;
+    }
+    cout << ans;
+    return 0;
+}
