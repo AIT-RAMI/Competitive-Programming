5aafc5fc9cf54ce286ab30d41e12669b8ca7a9e6
AIT-RAMI <mohamedaitrami@gmail.com>
2020-04-16 07:42:35 +0100

    Adding Trailing Zeros

---
+++ CSES/TrailingZeros.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1000000007;
+
+#define pf(n) cout << n << endl
+#define pfs(n) cout << n
+#define ps() cout << " "
+#define sf(n) cin >> n
+#define in(i,n) for (int i = 0; i < n; i++)
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define graph vector< vector<int> >
+#define vs vector<string>
+const int MAXN = 100005;
+/**  '' "Lionel VB" ^ **/
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int ans;
+    int fives = 5;
+    for(int i = 1; i <= 12; i++){
+        ans += n/fives;
+        fives *= 5;
+    }
+    cout << ans;
+    return 0;
+}
