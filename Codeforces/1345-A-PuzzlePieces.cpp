74a4db0bf62b6caf4128c6ee22741b01e4bacda6
AIT-RAMI <mohamedaitrami@gmail.com>
2020-05-06 17:15:56 +0000

    Adding Puzzle Pieces

---
+++ Codeforces/1345-A-PuzzlePieces.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1000000007;
+
+#define pf(n) cout << n << endl
+#define pfs(n) cout << n
+#define ps() cout << " "
+#define sf(n) cin >> n
+#define in(i,n) for (int i = 0; i < n; i++)
+#define vi vector<int>
+#define vp vector<pair<int, int>>
+#define graph vector< vector<int> >
+#define vs vector<string>
+const int MAXN = 100005;
+/**  '' "Lionel VB" ^ **/
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t, n, m; cin >> t;
+    while(t--){
+        cin >> n >> m;
+        if(n == 1 || m == 1) {
+            cout << "YES" << endl;
+            continue;
+        }
+        if(n == 2 && m == 2){
+            cout << "YES" << endl;
+            continue;
+        }
+        cout << "NO" << endl;
+    }
+
+
+    return 0;
+}
